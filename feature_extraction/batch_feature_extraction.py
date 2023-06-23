49cc92c3c1addbecb21faddd9f29edf416a7b868
flanneryjn <57923541+flanneryjn@users.noreply.github.com>
2023-06-23 15:42:27 +0200

    Update batch_feature_extraction.py
    Fixed command line inputs

--- feature_extraction/batch_feature_extraction.py
+++ feature_extraction/batch_feature_extraction.py
@@ -12,19 +12,17 @@ import SimpleITK as sitk
 CLI = argparse.ArgumentParser()
 CLI.add_argument(
     "--n_mris",
-    nargs="*",
     type=int,
     default=n_mris,
 )
 CLI.add_argument(
     "--output_directory",
-    nargs="*",
     type=str,
     default=data_directory,
 )
 args = CLI.parse_args()
-n_mris = args.n_mris[0]
-data_directory = args.output_directory[0]
+n_mris = args.n_mris
+data_directory = args.output_directory
 
 folders = [os.path.join(bids, folder) for folder in os.listdir(bids) if os.path.isdir(os.path.join(bids, folder))]
 files_visited = 0
@@ -41,7 +39,7 @@ for folder in folders:
         # make sure we have needed files, try loading our MRI
         ica_time_series_file = patient + '_ses-2_task-rest_ts-ica-25.txt'
         base_mri = patient + '_ses-2_task-rest_filtered-clean.nii.gz'
-        features_file = data_directory + 'feature_dicts/' + patient + '_ses-2_task-rest_network_features.json'
+        features_file = os.path.join(data_directory,'feature_dicts/' + patient + '_ses-2_task-rest_network_features.json')
         if not exists(features_file):
             brain = image.load_img(base_mri)
     except:
@@ -55,8 +53,8 @@ for folder in folders:
             print("Current Time =", current_time)
 
         ## Then we name the files we will create with FSL
-        patient_inverse_warp_field = data_directory + patient + '_ses-2_task-rest_mni2func-warp.nii.gz'
-        inverse_brainnetome = data_directory + patient + '_ses-2_task-rest_inverse_brainnetome.nii.gz'
+        patient_inverse_warp_field = os.path.join(data_directory, patient + '_ses-2_task-rest_mni2func-warp.nii.gz')
+        inverse_brainnetome = os.path.join(data_directory, patient + '_ses-2_task-rest_inverse_brainnetome.nii.gz')
 
         ##using FSL command line, inverse a warp and apply it to the brainnetome atlas, only when this file doesn't already exist
         if not exists(inverse_brainnetome):
@@ -89,20 +87,20 @@ for folder in folders:
         lobe_time_series = lobe_time_series.groupby(['Lobe']).mean()
 
         ## Collect features
-        features = {}
         try:
+            features = {}
             features['Total Probabilistic Voxel Volume In Target Regions'] = np.sum(regions['vol'])
             features['Total Probabalistic Voxel Volume Proportional To Atlas Volume'] = \
                 np.sum(regions['vol']) / np.sum(brainnetome_lobe_vol['vol'])
+            ica_features = ICA_graph_feature_extraction(ica_time_series_file, THRESHOLDS, valid_ica_regions, return_correlations)
             gyri_time_series_features = atlas_time_series_feature_extraction(gyri_time_series, THRESHOLDS, \
-                                                                             return_graph_features, return_correlations)
+                                                                                False, False)
             lobe_time_series_features = atlas_time_series_feature_extraction(lobe_time_series, THRESHOLDS, \
-                                                                             return_graph_features, return_correlations)
+                                                                                return_graph_features, return_correlations)
             gyri_volume_features = region_feature_extraction(gyri_vol, brainnetome_gyri_vol)
             lobe_volume_features = region_feature_extraction(lobe_vol, brainnetome_lobe_vol)
-            ica_features = ICA_graph_feature_extraction(ica_time_series_file, THRESHOLDS, valid_ica_regions, return_correlations)
             for sub_features in [gyri_time_series_features, lobe_time_series_features, gyri_volume_features, \
-                                 lobe_volume_features, ica_features]:
+                                    lobe_volume_features, ica_features]:
                 features.update(sub_features)
             ## Write features to output
             with open(features_file, 'w') as data:
@@ -113,6 +111,7 @@ for folder in folders:
             failed_extraction_ids.append(patient)
             print('extraction failed for ' + str(patient))
 
+
     if files_visited >= n_mris:
         break
     if folders_without_necessary_files == max_files:
@@ -120,5 +119,5 @@ for folder in folders:
 if verbose:
     print(str(files_visited) + ' MRIs were processed for feature extraction')
     print(str(folders_without_necessary_files) + " MRI folders were visited but they lacked necessary files for extraction")
-    print(str(feature_extraction_failures) + 'MRIs had an error in feature extraction, their IDs were:')
+    print(str(feature_extraction_failures) + ' MRIs had an error in feature extraction, their IDs were:')
     print(failed_extraction_ids)
